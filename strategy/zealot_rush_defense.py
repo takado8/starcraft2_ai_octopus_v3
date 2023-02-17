0def199764a0fdf902afe378022bd343aa810915
takado <kornelgut@gmail.com>
2023-02-17 02:33:09 +0100

    fix

--- strategy/zealot_rush_defense.py
+++ strategy/zealot_rush_defense.py
@@ -39,37 +39,37 @@ class ZealotRushDefense(StrategyABS):
         disruptor_micro = DisruptorMicro(ai)
         colossus_micro = ColossusMicro(ai)
         # dt_micro = DarkTemplarMicro(ai)
-        # self.army.create_division('wall_guard_zealots', {unit.ZEALOT: 3}, [wall_guard_zealot_micro],
-        #                           Movements(ai, 0.33), lifetime=400)
-        # self.army.create_division('zealots', {unit.ZEALOT: 10}, [zealot_micro], Movements(ai, 0.1))
-        # self.army.create_division('zealots2', {unit.ZEALOT: 5}, [zealot_micro], Movements(ai, 0.1))
-        #
-        # self.army.create_division('adepts2', ADEPT_x5, [adept_micro], Movements(ai, 0.2))
-        # self.army.create_division('adepts3', ADEPT_x5, [adept_micro], Movements(ai, 0.2))
-        #
-        # # self.army.create_division('dt', {unit.DARKTEMPLAR: 2}, [dt_micro], Movements(ai, 0.1))
-        # # self.army.create_division('dt', {unit.DARKTEMPLAR: 2}, [dt_micro], Movements(ai, 0.1))
-        # # self.army.create_division('adepts6', ADEPT_x5, [adept_micro], Movements(ai, 0.2))
-        # # self.army.create_division('sentry1', SENTRY_x1, [sentry_micro], Movements(ai, 0.2))
-        # self.army.create_division('immortals1', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
-        # self.army.create_division('immortals2', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
-        # # self.army.create_division('immortals3', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
-        # # self.army.create_division('immortals3', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
-        #
-        # self.army.create_division('archons1', ARCHONS_x5, [archon_micro], Movements(ai, 0.2))
-        # self.army.create_division('archons2', ARCHONS_x5, [archon_micro], Movements(ai, 0.2))
-        #
-        # self.army.create_division('stalkers1', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
-        # self.army.create_division('stalkers2', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
-        # self.army.create_division('stalkers3', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
-        # self.army.create_division('stalkers4', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
-        #
-        # self.army.create_division('sentry2', SENTRY_x1, [sentry_micro], Movements(ai, 0.2), lifetime=-260)
-        # self.army.create_division('sentry3', SENTRY_x1, [sentry_micro], Movements(ai, 0.2), lifetime=-260)
-        # self.army.create_division('observer', OBSERVER_x1, [], Movements(ai, 0.2))
-        # self.army.create_division('warpprism', WARPPRISM_x1, [warpprism_micro], Movements(ai, 0.2), lifetime=-360)
-        # self.army.create_division('colossi', {unit.COLOSSUS: 2}, [colossus_micro], Movements(ai, 0.2), lifetime=-420)
-        # self.army.create_division('disruptors', {unit.DISRUPTOR: 3}, [disruptor_micro], Movements(ai, 0.2), lifetime=-420)
+        self.army.create_division('wall_guard_zealots', {unit.ZEALOT: 3}, [wall_guard_zealot_micro],
+                                  Movements(ai, 0.33), lifetime=400)
+        self.army.create_division('zealots', {unit.ZEALOT: 10}, [zealot_micro], Movements(ai, 0.1))
+        self.army.create_division('zealots2', {unit.ZEALOT: 5}, [zealot_micro], Movements(ai, 0.1))
+
+        self.army.create_division('adepts2', ADEPT_x5, [adept_micro], Movements(ai, 0.2))
+        self.army.create_division('adepts3', ADEPT_x5, [adept_micro], Movements(ai, 0.2))
+
+        # self.army.create_division('dt', {unit.DARKTEMPLAR: 2}, [dt_micro], Movements(ai, 0.1))
+        # self.army.create_division('dt', {unit.DARKTEMPLAR: 2}, [dt_micro], Movements(ai, 0.1))
+        # self.army.create_division('adepts6', ADEPT_x5, [adept_micro], Movements(ai, 0.2))
+        # self.army.create_division('sentry1', SENTRY_x1, [sentry_micro], Movements(ai, 0.2))
+        self.army.create_division('immortals1', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
+        self.army.create_division('immortals2', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
+        # self.army.create_division('immortals3', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
+        # self.army.create_division('immortals3', IMMORTAL_x2, [immortal_micro], Movements(ai, 0.2))
+
+        self.army.create_division('archons1', ARCHONS_x5, [archon_micro], Movements(ai, 0.2))
+        self.army.create_division('archons2', ARCHONS_x5, [archon_micro], Movements(ai, 0.2))
+
+        self.army.create_division('stalkers1', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
+        self.army.create_division('stalkers2', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
+        self.army.create_division('stalkers3', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
+        self.army.create_division('stalkers4', STALKER_x5, [stalker_micro], Movements(ai, 0.5))
+
+        self.army.create_division('sentry2', SENTRY_x1, [sentry_micro], Movements(ai, 0.2), lifetime=-260)
+        self.army.create_division('sentry3', SENTRY_x1, [sentry_micro], Movements(ai, 0.2), lifetime=-260)
+        self.army.create_division('observer', OBSERVER_x1, [], Movements(ai, 0.2))
+        self.army.create_division('warpprism', WARPPRISM_x1, [warpprism_micro], Movements(ai, 0.2), lifetime=-360)
+        self.army.create_division('colossi', {unit.COLOSSUS: 2}, [colossus_micro], Movements(ai, 0.2), lifetime=-420)
+        self.army.create_division('disruptors', {unit.DISRUPTOR: 3}, [disruptor_micro], Movements(ai, 0.2), lifetime=-420)
 
         build_queue = BuildQueues.ZEALOT_RUSH_DEFENSE
         upper_wall = UpperWallGates(ai)
