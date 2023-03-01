138b2c1103c2e2f19f448765195549b54ebf4747
takado <kornelgut@gmail.com>
2023-03-01 02:28:38 +0100

    Update special_building_locations.py

--- builders/special_building_locations.py
+++ builders/special_building_locations.py
@@ -36,3 +36,17 @@ class UpperWallGates(SpecialBuildingsLocationsABS):
                 unit.SHIELDBATTERY: [self.ai.main_base_ramp.protoss_wall_buildings[1].towards(
                     self.ai.main_base_ramp.protoss_wall_buildings[0], -2.5)]
                 }
+
+
+class UpperWallLockdown(SpecialBuildingsLocationsABS):
+    def __init__(self, ai):
+        super().__init__(ai)
+
+    @property
+    def locations_dict(self):
+        return {unit.PYLON: [self.ai.main_base_ramp.protoss_wall_warpin],
+                unit.GATEWAY: [self.ai.main_base_ramp.protoss_wall_buildings[0],
+                               self.ai.main_base_ramp.protoss_wall_buildings[1]],
+                unit.SHIELDBATTERY: [self.ai.main_base_ramp.protoss_wall_buildings[1].towards(
+                    self.ai.main_base_ramp.protoss_wall_buildings[0], -2.5)]
+                }
