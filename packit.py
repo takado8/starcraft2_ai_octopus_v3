dcb7b28febebc96de8b0dd26c04eedb9ecf8ffa3
takado <kornelgut@gmail.com>
2023-06-08 23:18:13 +0200

    Update packit.py

--- packit.py
+++ packit.py
@@ -5,15 +5,13 @@ octopus = 'OctopusV3'
 aaaaaaa = 'aaaaaaa'
 
 zip_file_name = octopus
-
 destination_dir_name = zip_file_name # could be different, is the same usually
-
-destination_dir = os.path.join(r"C:\Users\korne\Desktop\sc2 AI", destination_dir_name)
-source_dir = r"C:\Users\korne\Desktop\sc2 AI\starcraft2_ai_octopus_v3"
 zip_dir = r"C:\Users\korne\Desktop\sc2 AI"
 
-ignored_names = {'.git', '.gitattributes', '.gitignore', '.idea', 'packit.py', 'README.md', 'venv', '__pycache__'}
+destination_dir = os.path.join(zip_dir, destination_dir_name)
+source_dir = os.path.join(zip_dir, 'starcraft2_ai_octopus_v3')
 
+ignored_names = {'.git', '.gitattributes', '.gitignore', '.idea', 'packit.py', 'README.md', 'venv', '__pycache__'}
 
 all_files = os.listdir(source_dir)
 
@@ -41,9 +39,7 @@ except FileNotFoundError:
     print('no file at {}'.format(zip_file_path_ext))
 
 name = shutil.make_archive(base_name=zip_file_path, format='zip', root_dir=destination_dir)
-
 print('archive made at {}'.format(name))
-# shutil.move(zip_file_path_ext, zip_destination_path)
 
 if __name__ == '__main__':
     print(all_files)
