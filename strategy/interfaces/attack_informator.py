ae9aa3d079b2016157bc0495c60193668cf7c542
takado <kornelgut@gmail.com>
2023-06-16 00:44:04 +0200

    Update attack_informator.py

--- strategy/interfaces/attack_informator.py
+++ strategy/interfaces/attack_informator.py
@@ -3,7 +3,6 @@ from sc2.unit import UnitTypeId as unit
 from sc2.ids.upgrade_id import UpgradeId as upgrade
 
 
-
 class AttackInformant(InterfaceABS):
 
     def __init__(self, ai):
@@ -11,13 +10,19 @@ class AttackInformant(InterfaceABS):
         self.previous_attack_state = False
 
     async def execute(self):
-        if self.ai.attack != self.previous_attack_state or self.ai.attack and self.ai.iteration % 100 == 0:
+        if self.ai.attack != self.previous_attack_state or (self.ai.attack and self.ai.iteration % 100 == 0) or \
+                (self.ai.iteration % 500 == 0 and self.ai.iteration > 0):
+
             self.previous_attack_state = self.ai.attack
-            if self.ai.attack:
-                word = 'Attacking'
+            if self.ai.iteration % 500 == 0:
+                word = 'Chillin\''
             else:
-                word = 'Retreating'
-            msg = "{} with army value {} vs {}".format(word, self.ai.strategy.own_economy.army_value,
-                                self.ai.strategy.enemy_economy.army_value)
+                if self.ai.attack:
+                    word = 'Attacking'
+                else:
+                    word = 'Retreating'
+            fitness = self.ai.strategy.army_fitness.get_army_fitness()
+            msg = "{} with army value {} vs {}, fit {} vs {}".format(word, self.ai.strategy.own_economy.army_value,
+                                self.ai.strategy.enemy_economy.army_value, fitness[0], fitness[1])
             print(msg)
             await self.ai.chat_send(msg)
