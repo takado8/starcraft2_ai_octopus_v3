80caed55b0589c560bd67edc7dbb3e7bbf4e0387
takado <kornelgut@gmail.com>
2023-09-08 00:13:03 +0200

    Update workers_micro.py

--- economy/workers/workers_micro.py
+++ economy/workers/workers_micro.py
@@ -11,9 +11,12 @@ class WorkersMicro:
         enemy.extend(self.ai.enemy_structures(unit.BUNKER))
         if enemy:
             for worker in workers:
-                if worker.shield_percentage < 1:
+                close_enemy = enemy.closer_than(12, worker)
+                if close_enemy and worker.shield_percentage < 1 or close_enemy.amount >= 3 or \
+                        close_enemy.filter(lambda x: x.type_id in {unit.WIDOWMINE, unit.WIDOWMINEBURROWED} and
+                                     x.distance_to(worker) <= 6):
                     if self.ai.townhalls.amount == 1:
-                        worker.move(enemy.closest_to(worker, -6))
+                        worker.move(worker.position.towards(enemy.closest_to(worker), -6))
                     else:
                         safe_townhall = self.ai.townhalls.filter(lambda x: not enemy.closer_than(12, x).exists)
-                        worker.move(safe_townhall.closest_to(worker))
+                        worker.gather(self.ai.mineral_field.closest_to(safe_townhall.closest_to(worker)))
