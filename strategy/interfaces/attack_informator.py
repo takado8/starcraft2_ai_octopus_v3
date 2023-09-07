cc9f1dd4bec7d691c13847d3b7a6bd3c258d6055
takado <kornelgut@gmail.com>
2023-09-08 00:12:53 +0200

    Update attack_informator.py

--- strategy/interfaces/attack_informator.py
+++ strategy/interfaces/attack_informator.py
@@ -1,6 +1,4 @@
 from strategy.interfaces.interfaceABS import InterfaceABS
-from sc2.unit import UnitTypeId as unit
-from sc2.ids.upgrade_id import UpgradeId as upgrade
 
 
 class AttackInformant(InterfaceABS):
@@ -10,17 +8,12 @@ class AttackInformant(InterfaceABS):
         self.previous_attack_state = False
 
     async def execute(self):
-        if self.ai.attack != self.previous_attack_state or (self.ai.attack and self.ai.iteration % 100 == 0) or \
-                (self.ai.iteration % 500 == 0 and self.ai.iteration > 0):
-
+        if self.ai.attack != self.previous_attack_state or (self.ai.attack and self.ai.iteration % 100 == 0):
             self.previous_attack_state = self.ai.attack
-            if self.ai.iteration % 500 == 0:
-                word = 'Chillin\''
+            if self.ai.attack:
+                word = 'Attacking'
             else:
-                if self.ai.attack:
-                    word = 'Attacking'
-                else:
-                    word = 'Retreating'
+                word = 'Retreating'
             fitness = self.ai.strategy.army_fitness.get_army_fitness()
             msg = "{} with army value {} vs {}, fit -{} vs -{}".format(word, round(self.ai.strategy.own_economy.army_value),
                                 round(self.ai.strategy.enemy_economy.army_value), round(fitness[0]), round(fitness[1]))
