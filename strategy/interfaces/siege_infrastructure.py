fd8116373d15045592bd848c297255e97b125e37
takado <kornelgut@gmail.com>
2023-09-08 22:12:50 +0200

    Update siege_infrastructure.py

--- strategy/interfaces/siege_infrastructure.py
+++ strategy/interfaces/siege_infrastructure.py
@@ -10,9 +10,9 @@ class SiegeInfrastructure(InterfaceABS):
         self.siege_location = None
 
     async def execute(self):
-        if self.ai.attack and self.ai.supply_army > 60:
+        if self.ai.attack:
             if not self.siege_location:
-                self.siege_location = self.ai.game_info.map_center.towards(self.ai.enemy_start_locations[0], 20)
+                self.siege_location = self.ai.game_info.map_center.towards(self.ai.enemy_start_locations[0], 10)
                 self.siege_location = await self.ai.find_placement(unit.PYLON, near=self.siege_location)
 
 
@@ -27,14 +27,15 @@ class SiegeInfrastructure(InterfaceABS):
                 workers = self.ai.workers.filter(lambda x: x.tag in workers_tags)
             if workers:
                 builder = workers.closest_to(self.siege_location)
-                if builder.distance_to(self.siege_location) > 25:
+                if builder.distance_to(self.siege_location) > 20:
                     builder.move(self.siege_location)
                 elif isinstance(builder.order_target, int):
                     builder.move(self.siege_location)
-                if builder.is_moving and self.ai.minerals > 500:
+                if self.ai.supply_army >= 60 and builder.is_moving and self.ai.minerals > 500 and (not self.ai.enemy_units() or
+                        not self.ai.enemy_units().closer_than(10, self.siege_location).exists):
                     pylons = self.ai.structures().filter(lambda x: x.type_id == unit.PYLON
                                                                  and x.distance_to(self.siege_location) <= 14)
-                    if pylons.amount < 3 and self.ai.already_pending(unit.PYLON) < 2:
+                    if pylons.amount < 2 and self.ai.already_pending(unit.PYLON) < 2:
                         await self.ai.build(unit.PYLON, near=self.siege_location, build_worker=builder, queue=True)
                     pylons_rdy = pylons.ready
                     if pylons_rdy:
