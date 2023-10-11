02d0718781aa7683672d12a2d3d8d7d955ace0a2
takado <kornelgut@gmail.com>
2023-10-11 16:37:11 +0200

    Revert "Update observer.py"
    This reverts commit 91facb277e775ece1a9e216a99c6a62df5ff41e3.

--- army/micros/observer.py
+++ army/micros/observer.py
@@ -12,7 +12,6 @@ class ObserverMicro(MicroABS):
         super().__init__('ObserverMicro', ai)
 
     async def do_micro(self, division):
-        all_observers = self.ai.units(unit.OBSERVER)
         observers = division.get_units(self.ai.iteration, unit.OBSERVER)
         all_enemy = self.ai.enemy_units()
         for observer in observers:
@@ -24,18 +23,21 @@ class ObserverMicro(MicroABS):
 
                 if detectors.exists:
                     detector = detectors.closest_to(observer)
-                    observer.move(observer.position.towards(detector, -7))
+                    observer.move(observer.position.towards(detector, -4))
                     continue
 
-            if all_enemy.exists:
+                enemy = self.ai.enemy_units().filter(lambda x: x.type_id not in self.ai.units_to_ignore
+                                                               and x.distance_to(observer) < 10)
+
+                if enemy.exists and observer.is_idle:
+                    enemy_center = enemy.center
+                    radius = enemy_center.distance_to(enemy.furthest_to(enemy_center)) + 3
+                    points = points_on_circumference_sorted(enemy_center, observer.position, radius, n=10)
+                    for point in points:
+                        observer.move(point, queue=True)
+            else:
                 invisible_threats = all_enemy.filter(lambda x: not x.has_buff(buff.ORACLEREVELATION) and
-                                        (x.cloak in {1, 2} or x.type_id in BURROWING_UNITS_IDS) and
-                                                               (all_observers.closer_than(7, x).amount == 0 or
-                                                                all_observers.closest_to(x).tag == observer.tag))
+                                                           (x.cloak in {1, 2} or x.type_id in BURROWING_UNITS_IDS))
                 if invisible_threats:
-                    observer.move(invisible_threats.closest_to(observer.position))
-                    continue
-            division_position = division.get_position(self.ai.iteration)
-            if division_position and observer.is_idle:
-                observer.move(division_position)
+                    observer.move(observer.position.towards(invisible_threats.closest_to(observer.position), 4))
         return observers.amount
