ee3f008c91073087195aef0f826a977611fdfe45
takado <kornelgut@gmail.com>
2023-10-24 22:09:58 +0200

    Update handle_proxy.py

--- strategy/interfaces/handle_proxy.py
+++ strategy/interfaces/handle_proxy.py
@@ -8,23 +8,30 @@ class HandleProxy(InterfaceABS):
     def __init__(self, ai):
         self.ai = ai
         self.last_scout_time = -10
-        self.scout_every_n_seconds = 30
+        self.scout_every_n_seconds = 20
         self.workers_amount = 5 #if self.ai.enemy_race == Race.Protoss else 3
         self.is_reported = False
         self.is_active = True
+        self.proxy_position = None
 
 
     async def execute(self):
         if self.ai.time < 360 and self.is_active:
+
             if self.ai.time < 240 and self.ai.time - self.last_scout_time > self.scout_every_n_seconds:
                 workers_tags = self.ai.strategy.workers_distribution.get_distant_mining_workers_tags()
                 if not workers_tags:
                     workers_tags = self.ai.strategy.workers_distribution.get_mineral_workers_tags()
                 if workers_tags:
                     workers = self.ai.workers.filter(lambda x: x.tag in workers_tags)
-                    scout = workers.closer_than(50, self.ai.start_location).furthest_to(self.ai.start_location)
+                else:
+                    workers = self.ai.units(unit.PROBE)
+                close_workers = workers.closer_than(50, self.ai.start_location)
+                if close_workers:
+                    scout = close_workers.furthest_to(self.ai.start_location)
+                
                     nearby_expansions = sorted(self.ai.expansion_locations_list,
-                                               key=lambda x: self.ai.start_location.distance_to(x))[1:3]
+                                               key=lambda x: self.ai.start_location.distance_to(x))[1:5]
                     scout.stop()
                     for exp in nearby_expansions:
                         scout.move(exp, queue=True)
@@ -34,11 +41,11 @@ class HandleProxy(InterfaceABS):
                 lambda x: x.type_id in {unit.BUNKER, unit.BARRACKS, unit.PYLON, unit.PHOTONCANNON} and
                           x.distance_to(self.ai.main_base_ramp.bottom_center) < 45)
             if enemy_proxy.exists:
+                self.make_probes()
                 if not self.is_reported:
                     self.is_reported = True
+                    self.proxy_position = enemy_proxy.closest_to(self.ai.start_location)
                     await self.ai.chat_send("Tag:2_handle proxy")
-                # barracs = enemy_proxy(unit.BARRACKS)
-                # bunkers = enemy_proxy(unit.BUNKER)
 
                 enemy_units = self.ai.enemy_units().closer_than(55, self.ai.main_base_ramp.bottom_center)
                 enemy_workers = enemy_units().filter(lambda x: x.type_id in {unit.SCV, unit.PROBE})
@@ -47,10 +54,11 @@ class HandleProxy(InterfaceABS):
                     closest_enemy = enemy_workers.closest_to(self.ai.main_base_ramp.bottom_center)
                     probes = self.ai.workers.closest_n_units(closest_enemy, self.workers_amount)
                     for probe in probes:
-                        if probe.distance_to(self.ai.main_base_ramp.bottom_center) > 60 or probe.shield_percentage < 0.1:
+                        if probe.distance_to(self.ai.main_base_ramp.bottom_center) > 60 or probe.shield_percentage < 0.35:
                             probe.gather(self.ai.mineral_field.closest_to(self.ai.start_location))
                         else:
                             probe.attack(closest_enemy)
+                    return
                 elif not enemy_units:
                     barracks = enemy_proxy(unit.BARRACKS)
                     if barracks:
@@ -70,3 +78,13 @@ class HandleProxy(InterfaceABS):
                 probes = self.ai.workers.closest_n_units(closest_enemy, self.workers_amount)
                 for probe in probes:
                     probe.attack(closest_enemy)
+            elif self.proxy_position:
+                if not self.ai.workers.closer_than(7, self.proxy_position):
+                    closest_probe = self.ai.workers.closest_to(self.proxy_position)
+                    closest_probe.move(self.proxy_position)
+
+    def make_probes(self):
+        if self.ai.workers.amount < 27:
+            nexus = self.ai.townhalls.first
+            if nexus.is_idle:
+                nexus.train(unit.PROBE)
